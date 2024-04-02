da7189d44ac8e6cf9114c06d2f5b24467f549d6c
iamharaprasad3 <110249992+iamharaprasad3@users.noreply.github.com>
2024-04-02 15:42:34 +0530

    Update check.py

--- check.py
+++ check.py
@@ -300,9 +300,9 @@ def runningmain(text_content, file_name, text):
         if "(work)" in period_type:
             start_month, start_year = work_period.split(" ")
             end_month, end_year = parts[1].split(" ")[0], parts[1].split(" ")[1]
+            end_year = end_year.replace(" (work) ")
+            end_year = end_year.replace(" (work)")
             end_year = end_year.replace("(work)", "")
-            end_year = end_year.replace("(education)", "")
-            end_year = end_year.replace("(internship)", "")
 
             duration = calculate_duration(start_month, int(start_year), end_month, int(end_year))
 
